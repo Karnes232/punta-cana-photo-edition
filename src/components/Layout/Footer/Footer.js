ca45598a6477070ec246d0bc43caa5771be544f4
James Karnes <karnes.james@gmail.com>
2024-09-23 18:20:01 -0400

    added instagram on footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Copyright from "./Copyright";
 import SocialMedia from "./SocialMedia";
-
+import { FaInstagram } from "react-icons/fa";
 const Footer = ({ generalInfo }) => {
   console.log(generalInfo);
+  const instatag = generalInfo.instagram.split("/")[3];
   return (
     <div className="flex flex-col h-screen">
-      <div></div>
+      <div className="flex justify-center items-center">
+        {generalInfo.instagram && (
+          <a
+            href={generalInfo.instagram}
+            target="_blank"
+            aria-label="Instagram"
+            rel="noreferrer"
+            className="flex flex-row items-center justify-center space-x-2 text-gray-400"
+          >
+            <FaInstagram className="text-2xl" />{" "}
+            <p className="uppercase text-sm tracking-widest">@{instatag}</p>
+          </a>
+        )}
+      </div>
       <div className="flex flex-col xl:flex-row justify-between">
         <div></div>
         <SocialMedia generalInfo={generalInfo} />
