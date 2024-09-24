ca594776b4c6cbbb90afa443f774662c88f493a1
James Karnes <karnes.james@gmail.com>
2024-09-24 18:21:51 -0400

    started service cards, adding gatsby photo, type of service and description

--- src/components/ServicesComponents/ServiceCard.js
+++ src/components/ServicesComponents/ServiceCard.js
@@ -7,22 +7,22 @@ const ServiceCard = ({ service }) => {
   const image = getImage(service.cardImage);
   return (
     <div className="flex flex-col w-11/12 md:w-[20rem] justify-center items-center overflow-hidden shadow-lg xl:group">
-      <div className="w-full h-[30rem]">
+      <div className="w-full h-[30rem] md:h-[22rem]">
         <GatsbyImage
           image={image}
           alt={service.cardImage.title}
-          className="w-full object-cover h-full"
+          className="w-full object-cover object-center h-full"
           imgClassName=""
           objectPosition=""
         />
       </div>
       <TextComponent
         title={service.typeOfService}
-        className="mt-5 2xl:mb-2 2xl:mt-10 text-2xl md:text-3xl"
+        className="mt-5 2xl:mb-2 2xl:mt-10 text-2xl xl:text-3xl xl:h-8"
       />
       <TextComponent
         title={service.cardDescription}
-        className="my-5 2xl:mb-2 2xl:mt-10 text-xl md:text-2xl"
+        className="my-5 mx-10 h-20 2xl:mb-2 2xl:mt-10 text-lg font-crimson font-extralight xl:text-2xl uppercase"
       />
       {/* <HiOutlineArrowNarrowRight className="mb-10 text-4xl text-gray-400 xl:opacity-0 xl:group-hover:opacity-100 xl:transition-opacity duration-300"/> */}
     </div>
--- src/components/TextComponent/TextComponent.js
+++ src/components/TextComponent/TextComponent.js
@@ -13,12 +13,15 @@ const TextComponent = ({ title, paragraph, className, pClassName }) => {
         }}
         className="flex flex-col items-center justify-center text-center max-w-5xl mx-5 lg:p-2 xl:mx-auto"
       >
-        <h1 className={`font-crimson ${className}`}>{title}</h1>
-        <p
-          className={`font-crimson lg:text-lg text-gray-700 lg:mt-5 ${pClassName}`}
-        >
-          {paragraph}
-        </p>
+        {title && <h1 className={`font-crimson ${className}`}>{title}</h1>}
+
+        {paragraph && (
+          <p
+            className={`font-crimson lg:text-lg text-gray-700 lg:mt-5 ${pClassName}`}
+          >
+            {paragraph}
+          </p>
+        )}
       </motion.div>
     </div>
   );
