f6704416cce5ac77e625828de333019bb62145aa
James Karnes <karnes.james@gmail.com>
2024-11-06 19:42:57 -0400

    created thank you page for after client submits form

--- src/pages/contact/thankyou.js
+++ src/pages/contact/thankyou.js
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby";
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
+import HeroSwiper from "../../components/HeroSwiper/HeroSwiper";
 
 const ThankYou = ({ data }) => {
   const [name, setName] = useState("");
@@ -10,8 +11,9 @@ const ThankYou = ({ data }) => {
   }, []);
   return (
     <Layout generalInfo={data.allContentfulGeneralLayout.nodes[0]}>
+      <HeroSwiper heroInfo={data.allContentfulGeneralLayout.nodes[0]} />
       <main className="">
-        <div className="flex flex-col items-center max-w-xs xl:max-w-sm mx-auto">
+        <div className="flex flex-col items-center justify-center max-w-xs xl:max-w-sm mx-auto min-h-[50vh]">
           <div className="">
             <div className="flex flex-col justify-center items-center text-slate-600 ">
               <div className="text-2xl xl:text-4xl font-serif text-center mt-6">
@@ -21,7 +23,7 @@ const ThankYou = ({ data }) => {
               <div className="text-center text-sm xl:text-base mt-2 xl:mt-6">
                 Please feel free to{" "}
                 <a
-                  href={`mailto:test@test.com`}
+                  href={`mailto:${data.allContentfulGeneralLayout.nodes[0].email}`}
                   aria-label="Gmail"
                   rel="noreferrer"
                   className="underline"
@@ -46,9 +48,16 @@ export const query = graphql`
       nodes {
         companyName
         facebook
+        email
         instagram
         x
         telephone
+        heroImageList {
+          gatsbyImage(width: 4000, placeholder: BLURRED, formats: WEBP)
+          title
+        }
+        fullSize
+        heroHeading
       }
     }
   }
