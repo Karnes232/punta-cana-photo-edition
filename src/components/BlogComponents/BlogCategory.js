ee9bfcfad42d089f66d476b3a44109436a666ef0
James Karnes <karnes.james@gmail.com>
2025-03-02 20:02:12 -0400

    added links to blog categories

---
+++ src/components/BlogComponents/BlogCategory.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "gatsby";
+import useWindowWidth from "../../hooks/useWindowWidth";
+
+const BlogCategory = ({ title, gImage, url }) => {
+  const windowWidth = useWindowWidth();
+  let image = gImage?.gatsbyImage?.images?.fallback?.srcSet.split(",");
+  const imageSrc = [];
+  console.log(gImage);
+  image?.forEach((element) => {
+    const image = element.split(" ");
+    const imageObject = { imageSrc: image[0], imageWidth: image[1] };
+    imageSrc.push(imageObject);
+  });
+  let HeroStyles = {};
+  if (windowWidth < 1500) {
+    HeroStyles = {
+      backgroundImage:
+        "linear-gradient(45deg, rgba(245, 246, 252, 0.2), rgba(0, 0, 0, 0.6)), url(" +
+        imageSrc[1]?.imageSrc +
+        ")",
+    };
+  } else {
+    HeroStyles = {
+      backgroundImage:
+        "linear-gradient(45deg, rgba(245, 246, 252, 0.52), rgba(0, 0, 0, 0.53)), url(" +
+        imageSrc[2]?.imageSrc +
+        ")",
+    };
+  }
+  return (
+    <>
+      <Link to={`/blog/${url}`}>
+        <div className="w-80 h-60 mx-auto my-5 rounded-lg overflow-hidden shadow-lg">
+          <div
+            className={`h-full bg-center bg-no-repeat bg-cover`}
+            style={HeroStyles}
+          >
+            <h1
+              translate="no"
+              className="relative inline-block text-center text-white z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-yellowtail tracking-widest text-3xl md:text-4xl"
+            >
+              {title}
+            </h1>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default BlogCategory;
--- src/pages/blog/complete-guide-to-organizing-events-in-punta-cana/index.js
+++ src/pages/blog/complete-guide-to-organizing-events-in-punta-cana/index.js
@@ -1,14 +1,30 @@
 import { graphql } from "gatsby";
 import React from "react";
-import Seo from "../../components/Layout/seo";
-import Layout from "../../components/Layout/Layout";
-import HeroSwiper from "../../components/HeroSwiper/HeroSwiper";
-import RichText from "../../components/RichTextComponents/RichText";
+import Seo from "../../../components/Layout/seo";
+import Layout from "../../../components/Layout/Layout";
+import HeroSwiper from "../../../components/HeroSwiper/HeroSwiper";
+import RichText from "../../../components/RichTextComponents/RichText";
+import BlogCategory from "../../../components/BlogComponents/BlogCategory";
 const Index = ({ data }) => {
+  const categories = data.allContentfulBlogCategories.nodes;
   return (
     <Layout generalInfo={data.allContentfulGeneralLayout.nodes[0]}>
       <HeroSwiper heroInfo={data.allContentfulPageContent.nodes[0]} />
+      <div className="mt-5 md:mt-0"></div>
       <RichText context={data?.allContentfulPageContent?.nodes[0].paragraph1} />
+      <div className="flex flex-col xl:mt-5 md:flex-row md:flex-wrap md:justify-evenly  max-w-5xl xl:max-w-6xl mx-auto md:gap-5">
+        {categories.map((category, index) => {
+          console.log(category);
+          return (
+            <BlogCategory
+              key={index}
+              title={category.blogCategory}
+              gImage={category.blogImage}
+              url={category.url}
+            />
+          );
+        })}
+      </div>
     </Layout>
   );
 };
@@ -75,6 +91,16 @@ export const query = graphql`
         heroHeading
         paragraph1 {
           raw
+        }
+      }
+    }
+    allContentfulBlogCategories {
+      nodes {
+        blogCategory
+        url
+        blogImage {
+          gatsbyImage(width: 400, placeholder: BLURRED, formats: WEBP)
+          title
         }
       }
     }
