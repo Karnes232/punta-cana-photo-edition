792a148ddb9f4dc9bec2a1248a7ef9218cdb7d1c
James Karnes <karnes.james@gmail.com>
2025-03-04 19:52:16 -0400

    checking if list of blogs

--- src/components/BlogComponents/PostList.js
+++ src/components/BlogComponents/PostList.js
@@ -5,9 +5,13 @@ const PostList = ({ list }) => {
   return (
     <>
       <div className="flex flex-col md:flex-row md:flex-wrap md:justify-evenly md:space-x-10 max-w-5xl xl:max-w-6xl mx-auto">
-        {list.map((blog, index) => {
+        {list &&
+          list.map((blog, index) => {
+            return <RecommendationCard blog={blog} key={index} />;
+          })}
+        {/* {list.map((blog, index) => {
           return <RecommendationCard blog={blog} key={index} />;
-        })}
+        })} */}
       </div>
     </>
   );
