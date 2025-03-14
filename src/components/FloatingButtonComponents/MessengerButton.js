aceb333f582dc979a594f1530d681ab9da38498f
James Karnes <karnes.james@gmail.com>
2025-03-14 17:00:25 -0400

    added messenger button

---
+++ src/components/FloatingButtonComponents/MessengerButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaFacebookMessenger } from "react-icons/fa6";
+import { useStaticQuery, graphql } from "gatsby";
+const MessengerButton = () => {
+  const data = useStaticQuery(graphql`
+    query messengerQuery {
+      allContentfulGeneralLayout {
+        nodes {
+          messengerLink
+        }
+      }
+    }
+  `);
+  console.log(data.allContentfulGeneralLayout.nodes[0].messengerLink);
+  return (
+    <>
+      <a
+        href={data.allContentfulGeneralLayout.nodes[0].messengerLink}
+        aria-label="Messenger"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <button className="fixed z-50 flex px-2 justify-center items-center bottom-6 right-6 xl:right-10 rounded-full h-14 w-14 bg-transparent border text-[#007FFF] font-bold">
+          <FaFacebookMessenger size={32} />
+        </button>
+      </a>
+    </>
+  );
+};
+
+export default MessengerButton;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import { useImageProtection } from "../../hooks/useImageProtection";
+import MessengerButton from "../FloatingButtonComponents/MessengerButton";
 
 const Layout = ({ children, generalInfo }) => {
   useImageProtection();
   return (
     <div className="min-h-screen font-crimson flex flex-col justify-between bg-primary-bg-color">
       <Navbar />
+      <MessengerButton />
       {children}
       <Footer generalInfo={generalInfo} />
     </div>
