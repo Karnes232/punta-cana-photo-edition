e8500aa6669bd4d8446843984a2355a176b2c6e9
James Karnes <karnes.james@gmail.com>
2025-03-25 07:27:18 -0400

    updated schema

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,11 +1,11 @@
 export const schema = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  name: "Punta Cana Photo Edition",
+  name: "Sertuin Events",
   description:
     "A premier destination for professional photography, event planning, and bespoke wedding services, dedicated to creating unforgettable memories and flawless celebrations.",
-  url: "https://punta-cana-photo.netlify.app",
-  logo: "https://punta-cana-photo.netlify.app/_gatsby/image/43e00492c582e3f86c16fa415d4ffe94/f2a9499e6f62fb9cb632a4f97dd860d3/Logo%20-%20Punta%20Cana%20Photo%20Edition.webp?u=https%3A%2F%2Fimages.ctfassets.net%2Fvpskymlp6aa0%2FpKzEbbiqIVQrzq8SeaxPy%2Fe9ebf1c72c93c6b2a6f8d0551e165811%2FLogo_-_Punta_Cana_Photo_Edition.png&a=w%3D500%26h%3D548%26fm%3Dwebp%26q%3D75&cd=2024-09-14T18%3A08%3A05.302Z",
+  url: "https://sertuinevents.com",
+  logo: "https://sertuinevents.com/_gatsby/image/7e80b5ffc02630f8b0579099aa029a11/aadda68e70952a77b95097eb6e8d4a1d/logotipo%20sertuin%20events.webp?u=https%3A%2F%2Fimages.ctfassets.net%2Fvpskymlp6aa0%2FpKzEbbiqIVQrzq8SeaxPy%2F8fe23dd9429e712b8c681cb2d287056b%2Flogotipo_sertuin_events.png&a=w%3D500%26h%3D516%26fm%3Dwebp%26q%3D75&cd=2025-02-07T22%3A34%3A29.891Z",
   contactPoint: {
     "@type": "ContactPoint",
     telephone: "+18295222900",
@@ -17,5 +17,5 @@ export const schema = {
     addressLocality: "Punta Cana",
     addressCountry: "DO",
   },
-  sameAs: ["https://punta-cana-photo.netlify.app/"],
+  sameAs: ["https://sertuinevents.com/"],
 };
